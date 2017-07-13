7653a4967af9dca6828fde0acf68507912c966a3
Sajil Shakya <shakya.sajil@gmail.com>
2017-07-12 22:00:27 -0400

    created objects, added methods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-var cards=["queen","queen","king","king"];
+var cards=[
+  {rank:"queen",
+  suit:"hearts",
+  cardImage:"images/queen-of-hearts.png"},
+  {rank:"queen",
+  suit:"diamonds",
+  cardImage:"images/queen-of-diamonds.png"},
+  {rank:"king",
+  suit:"hearts",
+  cardImage:"images/king-of-hearts.png"},
+  {rank:"king",
+  suit:"diamonds",
+  cardImage:"images/king-of-diamonds.png"}
+];
 var cardsInPlay=[];
 var checkForMatch=function(){
   //if (cardsInPlay.length===2){
@@ -8,9 +21,11 @@ var checkForMatch=function(){
     alert("Sorry try again.");}
 }
 var flipCard=function(cardId){
-console.log("user flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-checkForMatch();
+  console.log(cards[cardId].cardImage);
+  console.log("user flipped" + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId.rank]);
+  checkForMatch();
 }
 flipCard(0);
 flipCard(2);
